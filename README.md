e33947bb472e10b2046ca5df4a019ecb629ffaef
Jared McDonald <jared.r.mcdonald@gmail.com>
2018-01-06 02:28:55 -0500

    initial commit, seems to work decently well

---
+++ README.md
@@ -0,0 +1,12 @@
+# schotter
+
+a web transcription of
+[georg nees's "schotter"](http://collections.vam.ac.uk/item/O221321/schotter-print-nees-georg/)
+
+![georg nees, "schotter"](/nees.jpg)
+
+```bash
+$ npm install
+$ npm start
+$ open http://localhost:1234
+```
---
+++ index.js
@@ -0,0 +1,41 @@
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+const SQUARES_PER_ROW = 12;
+const CANVAS_MARGIN = 50;
+ctx.lineWidth = 1;
+ctx.strokeStyle = "black";
+const { width, height } = canvas;
+const squareSize = Math.floor((width - CANVAS_MARGIN * 2) / SQUARES_PER_ROW);
+const numRows = Math.floor((height - CANVAS_MARGIN * 2) / squareSize);
+
+function drawSquare(x, y, rotation) {
+  // push the unadulterated context to retrieve later
+  ctx.save();
+  // move the origin to the middle of the square
+  ctx.translate(x + squareSize / 2, y + squareSize / 2);
+  // rotate around the new origin
+  ctx.rotate(rotation);
+  // draw the rectangle (keeping in mind where the origin is now)
+  ctx.strokeRect(-squareSize / 2, -squareSize / 2, squareSize, squareSize);
+  // restore the previous context
+  ctx.restore();
+}
+
+function schotter() {
+  ctx.clearRect(0, 0, width, height);
+  for (let row = 0; row < numRows; row += 1) {
+    for (let col = 0; col < SQUARES_PER_ROW; col += 1) {
+      let rotationDirection = Math.random() < 0.5 ? 1 : -1;
+      let xDisplacement = Math.random() * row;
+      let yDisplacement = Math.random() * row;
+      drawSquare(
+        CANVAS_MARGIN + col * squareSize + xDisplacement,
+        CANVAS_MARGIN + row * squareSize + yDisplacement,
+        Math.random() * row * 0.05 * rotationDirection
+      );
+    }
+  }
+}
+
+schotter();
+// setInterval(schotter, 100);
